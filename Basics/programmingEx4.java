75df7ac5eaf1751f2202c95675c90cf8493df88e
anku <ankitayadav1825@gmail.com>
2025-04-11 15:19:14 +0530

    hj

--- Basics/programmingEx4.java
+++ Basics/programmingEx4.java
@@ -1,86 +1,84 @@
-import java.util.*;
-public class programmingEx4 { 
-   /*   public static void main(String args[]){
-      Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt() ;
+// import java.util.*;
+// public class programmingEx4 { 
+//    /*   public static void main(String args[]){
+//       Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt() ;
 
-        System.out.print("inbetweenNum are : ");{
-            for ( a=a; a <=b ; a++){
-                System.out.print(" " +a);
-            }
-         a = prev;
-           curr = a++ ;
-          t = curr;
-           next = curr + prev ;
-           prev++;
+//         System.out.print("inbetweenNum are : ");{
+//             for ( a=a; a <=b ; a++){
+//                 System.out.print(" " +a);
+//             }
+//          a = prev;
+//            curr = a++ ;
+//           t = curr;
+//            next = curr + prev ;
+//            prev++;
 
-           System.out.println(" prime fibbo : " +next);
-    }
-        //System.out.println(b);
+//            System.out.println(" prime fibbo : " +next);
+//     }
+//         //System.out.println(b);
 
-        */ 
-    /*     // FIBBO SERIES- next value is sum of prev + next value , so for in this ques we have to find out the numbers which are prime and fibbo also so lets code 
-       static boolean isPrime(int num )
-{
-    if(num<2)return false ;
-    for(int i = 2; i<= num ; i++){
-        if(num % i ==0) return false;
+//         */ 
+//     /*     // FIBBO SERIES- next value is sum of prev + next value , so for in this ques we have to find out the numbers which are prime and fibbo also so lets code 
+//        static boolean isPrime(int num )
+// {
+//     if(num<2)return false ;
+//     for(int i = 2; i<= num ; i++){
+//         if(num % i ==0) return false;
 
-    }
-    return true;
-}    
-        static boolean isfibbo(int num){
-            int a = 0, b = 1 , c;
-            if (num ==0 || num==1)return true;
-            while (b<= num){
-                c = a+b ;
-                if(c==num)return true;
-                a = b;
-                b = c;
-            }
-            return false;
-        }
-          public static void main(String args[]){
-            Scanner sc = new Scanner(System.in);
-            System.out.print("enter the lower range : ");
-            int lower = sc.nextInt();
-            System.out.print("enter the upper range : ");
-            int upper = sc.nextInt();
-            System.out.print("primefibbo num are  : ");
+//     }
+//     return true;
+// }    
+//         static boolean isfibbo(int num){
+//             int a = 0, b = 1 , c;
+//             if (num ==0 || num==1)return true;
+//             while (b<= num){
+//                 c = a+b ;
+//                 if(c==num)return true;
+//                 a = b;
+//                 b = c;
+//             }
+//             return false;
+//         }
+//           public static void main(String args[]){
+//             Scanner sc = new Scanner(System.in);
+//             System.out.print("enter the lower range : ");
+//             int lower = sc.nextInt();
+//             System.out.print("enter the upper range : ");
+//             int upper = sc.nextInt();
+//             System.out.print("primefibbo num are  : ");
 
-            for(int i = lower; i<=upper; i++){
-                if(isPrime(i) && isfibbo(i)){
-                    System.out.print(i +" ");
+//             for(int i = lower; i<=upper; i++){
+//                 if(isPrime(i) && isfibbo(i)){
+//                     System.out.print(i +" ");
 
-                }
-            }
-  sc.close();
-*/
+//                 }
+//             }
+//   sc.close();
+// */
 
-/*     // code for harmonnic mean
-public static void main(String args[]){
-float sum;
-int i;
-sum=0.0f;
-Scanner sc = new Scanner(System.in);
-System.out.print("enter the value of n :");
-int n = sc.nextInt();
+// /*     // code for harmonnic mean
+// public static void main(String args[]){
+// float sum;
+// int i;
+// sum=0.0f;
+// Scanner sc = new Scanner(System.in);
+// System.out.print("enter the value of n :");
+// int n = sc.nextInt();
 
-for( i=1; i<=n; i++)
-{
-    sum = sum + 1/(float)i;
-    System.out.println("i = " +i);
-    System.out.println("sum: "+sum);
-}
-    */
-    // for fibbo prime 
-    public static void main(String args[]){
+// for( i=1; i<=n; i++)
+// {
+//     sum = sum + 1/(float)i;
+//     System.out.println("i = " +i);
+//     System.out.println("sum: "+sum);
+// }
+//     */
+//     // for fibbo prime 
+//     public static void main(String args[]){
         
-    }
-   }
-          }
-
+//     }
+//    }
     
     
 
