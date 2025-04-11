3eaecaf80d3e1d0062e8ac632d98d483bc03178c
anku <ankitayadav1825@gmail.com>
2025-04-11 15:21:01 +0530

    done

--- Basics/ArrayCC.java
+++ Basics/ArrayCC.java
@@ -8,14 +8,6 @@ public class ArrayCC {
 
 System.out.println("length of array = "+ marks.length);
 
-    //    Scanner sc = new Scanner(System.in);
-        //int phy ;
-       // phy = sc.nextInt();
-    //    marks[0]= sc.nextInt();//phy
-    //    marks[1]= sc.nextInt();//chem
-    //    marks[2]= sc.nextInt();//math
-    //    marks[3]= sc.nextInt();//bio
-
     //    System.out.println("phy :" +marks[0]);
     //    System.out.println("chem :" +marks[1]);
     //    System.out.println("math :" +marks[2]);
