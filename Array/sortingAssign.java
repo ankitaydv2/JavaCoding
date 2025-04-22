c4b65231102a88fa2f19fc84102c9e66dffd4654
anku <ankitayadav1825@gmail.com>
2025-04-22 14:26:02 +0530

    2D Array

---
+++ Array/sortingAssign.java
@@ -0,0 +1,30 @@
+package Array;
+import java.util.Arrays;
+
+public class sortingAssign {
+    // ishi ques ko bubble , selection , insertion sort se krna tha
+    public static void bubbleSort(int arr[]) {
+        for(int turn =0; turn <arr.length-1; turn ++){
+            for(int j = 0; j <arr.length-1-turn; j++){
+            if(arr[j] < arr[j+1]){
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+        }
+    }
+}
+    }
+    public static void printArr(int arr[]) {
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        
+    }
+    public static void main(String[] args) {
+        int arr [] = { 3, 6, 2, 1, 8, 7, 4, 5, 3,};
+        bubbleSort(arr);
+        printArr(arr);
+    }
+}
