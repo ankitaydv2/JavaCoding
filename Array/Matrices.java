bf042e7fbb32a0885f0540e9daa3c0641245ef68
anku <ankitayadav1825@gmail.com>
2025-04-29 11:19:02 +0530

    Matrices

--- Array/Matrices.java
+++ Array/Matrices.java
@@ -3,6 +3,18 @@ import java.util.*;
 
 
 public class Matrices {
+    public static boolean search(int matrix[][] , int key){
+        for(int i = 0 ; i<=matrix.length ; i++){
+            for(int j =0; j<matrix[0].length; j++){
+                if(matrix[i][j]==key){
+                    System.out.println("found at cell("+ i + ","+j+")");
+                    return true;
+                }
+            }
+        }
+        System.out.println("key not found");
+        return false;
+    }
     public static void main(String[] args) {
        int matrix[][] = new int [3][3];
        int n = matrix.length , m =matrix[0].length;
@@ -10,16 +22,17 @@ public class Matrices {
        Scanner sc = new Scanner(System.in);
         for(int i =0 ; i<n ; i++){
             for(int j =0 ; j<m; j++){
-                matrix[i][j] = sc.nextInt()
+                matrix[i][j] = sc.nextInt();
             }
         }
         // output
         for(int i =0; i<n; i++){
             for(int j =0; j<m; j++){
-                System.out.println(matrix[i][j] + " ");
+                System.out.print(matrix[i][j] + " ");
             }
-            System.out.println();
+            System.out.println(); 
         }
+        search(matrix, 5); 
+      
     }
-    //456g87h8gf5d4d5f67gy78hu
 }
