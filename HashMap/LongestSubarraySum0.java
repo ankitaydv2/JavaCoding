ee733bb21395359e045a52f992caae1e2b896674
ANKITA YADAV <YADAVANKITA39270@gmai.com>
2025-11-15 07:52:51 +0530

    HashMap implementation through questions

---
+++ HashMap/LongestSubarraySum0.java
@@ -0,0 +1,26 @@
+package HashMap;
+import java.util.*;
+
+public class LongestSubarraySum0 {
+    public static void main(String args[]){ //0(n)
+        int arr[] = {15, -2, 2, -8,  1, 7, 10, 23};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //(sum , idx)
+
+        int sum =0 ;
+        int len =0 ;
+
+        for(int i =0 ; i<arr.length ; i++){
+            sum += arr[i];
+            if(map.containsKey(sum)){
+                len = Math.max(len , i-map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+        System.out.println("Largest subarray with sum 0 : " + len);
+
+    }
+    
+}
--- HashMap/UnionNIntersec.java
+++ HashMap/UnionNIntersec.java
@@ -31,8 +31,6 @@ public class UnionNIntersec {
         }
         System.out.println("Intersection = " + count);
 
-    }
-
-     
+    }    
     
 }
